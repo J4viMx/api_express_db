c9c2598e1c3133d14e20a3850915e0353c123804
JaviMx <javimxmine@hotmail.com>
2022-05-09 17:43:57 -0500

    EndPoint para crear un nuevo explorer (metodo POST)

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ app.listen(port, () => {
   console.log(`Listening to requests on port ${port}`);
 });
 
+
 app.get('/explorers', async (req, res) => {
     const allExplorers =  await prisma.explorer.findMany({});
     res.json(allExplorers);
@@ -25,3 +26,14 @@ const id = req.params.id;
 const explorer = await prisma.explorer.findUnique({where: {id: parseInt(id)}});
 res.json(explorer);
 });
+
+app.post('/explorers', async (req, res) => {
+    const explorer = {
+      name: req.body.name,
+      username: req.body.username,
+      mission: req.body.mission
+     };
+    const message = 'Explorer creado.';
+    await prisma.explorer.create({data: explorer});
+    return res.json({message});
+  });
